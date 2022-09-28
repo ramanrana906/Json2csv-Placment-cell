b0260c69976bed72f21211910c2c56f533af6194
ramanrana906 <99517511+ramanrana906@users.noreply.github.com>
2022-09-28 19:29:47 +0530

    Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require('express');
 const env = require('./config/enviornment');
 const app = express();
 
-const port =  8000;
+const port =  process.env.PORT || 8000;
 const cookieParser = require('cookie-parser');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
